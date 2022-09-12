78c3fa78bf6547d433e09677ad3bc3a701e35005
celinaramos <112406630+celinaramos@users.noreply.github.com>
2022-09-11 18:37:08 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Surprised)
+    basic.pause(1000)
+})
